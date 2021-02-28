3491b644a29afeada850926e33af05b9707a884c
limjen427 <limjen427@gmail.com>
2021-02-28 13:29:00 -0600

    Redesign project

--- for_roommate/share_cal/share_cal/asgi.py
+++ for_roommate/share_cal/share_cal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for for_roommate project.
+ASGI config for share_cal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'for_roommate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'share_cal.settings')
 
 application = get_asgi_application()
--- for_roommate/share_gro/share_gro/urls.py
+++ for_roommate/share_gro/share_gro/urls.py
@@ -1,4 +1,4 @@
-"""for_roommate URL Configuration
+"""share_gro URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
